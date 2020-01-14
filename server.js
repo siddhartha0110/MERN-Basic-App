7b8c8117d7979bcf685a83936dd7ec8fb9995b65
Siddhartha Saxena <siddharthasaxena.1998@gmail.com>
2020-01-14 14:38:49 +0530

    Setting up React

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
 
 app.use("/api/items",items)
 
-const port=process.env.PORT||3000;
+const port=process.env.PORT||5000;
 app.listen(port,()=>console.log(`Server running on port ${port}`));
